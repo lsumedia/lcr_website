ed5c4c4b7b394854b751dde843801feab0d1b6c4
Cameron Small <camerontsmall@googlemail.com>
2016-11-13 01:11:46 +0000

    Cleaned directory, made netbeans project

--- README.md
+++
@@ -1,4 +0,0 @@
-# lcr_website
-Loughborough Campus Radio Website
-
-Heavily AJAX-oriented, designed for online audio streaming from the outset.
--- pages/error.php
+++
@@ -1,4 +0,0 @@
-<main class="container" id="main-content">
-    <h4>404 Error - Page not found</h4>
-    <p>The page you requested does not exist. Try <a href="javascript:void(0);" onclick="pages.loadPage('search');">searching</a> the site.</p>
-</main>
